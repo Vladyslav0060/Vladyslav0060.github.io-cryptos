c8e419c5052a34fd5423981d76ecbff297468d36
Vladislav0060 <vlad.bortnik1@gmail.com>
2022-01-28 12:52:39 +0200

    Updates

--- src/types/types.ts
+++
@@ -1,8 +0,0 @@
-export interface IFetchedData {
-  id: number;
-  name: string;
-  color: string;
-}
-export interface IAccessibility {
-  setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
-}
